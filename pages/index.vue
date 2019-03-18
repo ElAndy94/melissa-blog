eaee632333ae6f61324714e3e84162fe6e791612
Andrew <andrewpeliza@hotmail.com>
2019-03-18 14:48:59 +0000

    Adding a backend

--- pages/index.vue
+++ pages/index.vue
@@ -1,68 +1,58 @@
 <template>
-  <section class="container">
-    <div>
-      <logo />
-      <h1 class="title">
-        melissa-blog
-      </h1>
-      <h2 class="subtitle">
-        Melissa&#39;s Blog
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green"
-        >Documentation</a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >GitHub</a>
-      </div>
-    </div>
+  <section id="posts">
+    <PostPreview
+      v-for="post in posts"
+      :key="post.id"
+      :title="post.title"
+      :excerpt="post.previewText"
+      :thumbnailImage="post.thumbnailUrl"
+      :id="post.id"
+    />
   </section>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
+import PostPreview from "@/components/Blog/PostPreview";
 
 export default {
   components: {
-    Logo
+    PostPreview
+  },
+  data() {
+    return {
+      posts: [
+        {
+          title: "A New Beginning",
+          previewText: "This will be awesome, don't miss it!",
+          thumbnailUrl:
+            "https://images.unsplash.com/photo-1552685495-b6ea5d342f41?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1834&q=80",
+          id: "a-new-beginning"
+        },
+        {
+          title: "A Second Beginning",
+          previewText: "This will be awesome, don't miss it!",
+          thumbnailUrl:
+            "https://images.unsplash.com/photo-1552685495-b6ea5d342f41?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1834&q=80",
+          id: "a-second-beginning"
+        }
+      ]
+    };
   }
-}
+};
 </script>
 
 <style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
+#posts {
+  padding-top: 2rem;
   display: flex;
   justify-content: center;
   align-items: center;
-  text-align: center;
+  flex-direction: column;
 }
 
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
+@media (min-width: 35rem) {
+  #posts {
+    flex-direction: row;
+  }
 }
 </style>
